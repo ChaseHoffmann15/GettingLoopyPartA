e5abc8691ca36733ea809d5e3ce34b56a6534aad
ChaseHoffmann15 <chasehoffmann14@outlook.com>
2022-10-15 22:51:45 -0500

    Initial commit

---
+++ src/Lab07Loops.java
@@ -0,0 +1,9 @@
+public class Lab07Loops {
+    public static void main(String[] args) {
+
+        for (int counter = 1; counter >= 30; counter-=0){
+            System.out.print(counter + " ");
+        }
+
+    }
+}
